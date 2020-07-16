61ac452d3a21f24f239eaf1795afc5c54126cba6
Jack Domleo <jackdomleo7@gmail.com>
2020-07-16 15:28:19 +0100

    Deploy

---
+++ docs/precache-manifest.301bc1569d4aab929b3d1966d4c703f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5ed807099c5a046898",
+    "url": "/project-state-manager/css/app.6294829d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/project-state-manager/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0a03184ff0b5bdae6470c1268486d200",
+    "url": "/project-state-manager/index.html"
+  },
+  {
+    "revision": "28d7a392066945b24f8d",
+    "url": "/project-state-manager/js/about.35e3f0cf.js"
+  },
+  {
+    "revision": "2d5ed807099c5a046898",
+    "url": "/project-state-manager/js/app.b9cf334c.js"
+  },
+  {
+    "revision": "29c5c47ab11f214f6d66",
+    "url": "/project-state-manager/js/chunk-vendors.75975ebd.js"
+  },
+  {
+    "revision": "5f80781d4ca7cb4d6b089357307c1ef7",
+    "url": "/project-state-manager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/project-state-manager/robots.txt"
+  }
+]);
