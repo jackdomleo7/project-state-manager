2ac5fd7f2044f947e22c4d5febe5b4a393be48fc
Jack Domleo <jackdomleo7@gmail.com>
2020-07-26 22:06:01 +0100

    Temp

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    projects: [
+      {
+        number: "00001",
+        name: "HMO Long Eaton",
+        postcode: "NG10 1AA",
+        status: "live",
+        completionDate: new Date(),
+        email: "long.eaton@nottingham.com"
+      }
+    ]
+  },
+  getters: {
+    projectsCount () {
+      //...
+    }
+  },
+  actions: {
+    addProject (context: any, project: any) {
+      
+    }
+  },
+  mutations: {
+    setProjects (state: any, projects: any) { // project = payload
+      state.projects = projects;
+    }
+  }
 });
